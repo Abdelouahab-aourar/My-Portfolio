8805aa2968c8128eb4377ca54dea4a13e08e14fa
Abdelouahab_a <wahabaourar00@gmail.com>
2025-07-16 14:32:30 +0100

    .

---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <div>
+            <h1>AOURAR ABDELOUAHAB</h1>
+            <h1>AI STUDENT (AT ENSIA )</h1>
+        </div>
+    )
+}
+export default Header;
