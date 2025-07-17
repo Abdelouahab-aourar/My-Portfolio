3b1fdd49624a3d5494d859a7b2f3dc38d9f3ba78
Abdelouahab_a <wahabaourar00@gmail.com>
2025-07-17 19:45:13 +0100

    styling the scrollbar

--- src/Terminal.tsx
+++ src/Terminal.tsx
@@ -98,6 +98,6 @@ const TerminalComponent = () => {
     };
   }, []);
 
-  return <div ref={containerRef} className="h-[100%] scrollbar-rounded-full"/>;
+  return <div ref={containerRef} className="h-full w-full"/>;
 };
 export default TerminalComponent;
