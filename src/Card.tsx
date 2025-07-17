2f7b2f94971382cd2a114d41608a7c87ce129ef7
Abdelouahab_a <wahabaourar00@gmail.com>
2025-07-17 11:12:39 +0100

    finished styling card.tsx with the hover effect

--- src/Card.tsx
+++ src/Card.tsx
@@ -5,13 +5,13 @@ import logo from "./assets/logo.png"
 function Card(){
     return(
     <>
-        <div className="box w-80 items-center">
-            <div className="box rounded-full"><img src={logo} alt="My Logo" className="w-40"/></div>
-            <h1 className="text-[#00FE00]">CONTACT ME</h1>
+        <div className="box w-[40vw] h-[81vh] items-center mx-[0.5vw] my-[1vh] justify-center">
+            <div className="box rounded-full mb-[5vh]"><img src={logo} alt="My Logo" className="w-[20vw]"/></div>
+            <h1 className="text-[#00FE00] mb-[5vh] text-4xl">CONTACT ME</h1>
             <div className="flex items-center justify-center gap-x-6">
-                <a href="https://github.com/Abdelouahab-aourar" target="_blank"><img src={github} className="w-12"/></a>
-                <a href="https://www.instagram.com/abdelouahab_aourar/profilecard/?igsh=NnpmNGJ5b3M0YXhi" target="_blank"><img src={instagram} className="w-12"/></a>
-                <a href="https://www.linkedin.com/in/aourar-abdelouahab" target="_blank"><img src={linkedin} className="w-12"/></a>
+                <a href="https://github.com/Abdelouahab-aourar" target="_blank"><img src={github} className="w-[4vw] transition-transform duration-1000 ease-linear hover:scale-130"/></a>
+                <a href="https://www.instagram.com/abdelouahab_aourar/profilecard/?igsh=NnpmNGJ5b3M0YXhi" target="_blank"><img src={instagram} className="w-[4vw] transition-transform duration-1000 ease-linear hover:scale-130"/></a>
+                <a href="https://www.linkedin.com/in/aourar-abdelouahab" target="_blank"><img src={linkedin} className="w-[4vw] transition-transform duration-1000 ease-linear hover:scale-130"/></a>
             </div>
         </div>
         
