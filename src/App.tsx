b37830f50e4e003959463a8a44bec90970146621
Abdelouahab_a <wahabaourar00@gmail.com>
2025-07-18 16:49:56 +0100

    modification

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="flex items-center  mx-[0.5vw] max-md:flex-col">
-        <Header title="AOURAR ABDELOUAHAB" subtitle="AI STUDENT" className="text-left pl-[1vw]" lengths="w-[40vw] mr-[0.5vw] max-md:w-[92vw]" />
+        <Header title="AOURAR ABDELOUAHAB" subtitle="AI STUDENT (ENSIA)" className="text-left pl-[1vw]" lengths="w-[40vw] mr-[0.5vw] max-md:w-[92vw]" />
         <Header title="AVAILABLE COMMANDS" subtitle="help | about | projects | skills | contact | clear" lengths="w-[60vw] max-md:w-[92vw]" />
       </div>
       <div className="flex items-center  mx-[0.5vw] max-md:flex-col">
