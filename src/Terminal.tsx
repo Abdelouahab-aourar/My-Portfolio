4a360471580f8dc29bf763775b2306673adb1ddd
Abdelouahab_a <wahabaourar00@gmail.com>
2025-07-18 16:23:52 +0100

    underline the links

--- src/Terminal.tsx
+++ src/Terminal.tsx
@@ -11,6 +11,8 @@ import "@xterm/xterm/css/xterm.css";
       yellow: "\x1b[33m",
       red: "\x1b[31m",
     };
+const underline = "\x1b[4m";
+const reset = "\x1b[0m";
 const TerminalComponent = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
@@ -34,7 +36,10 @@ const TerminalComponent = () => {
                       \r\n${C.red}contact${C.reset} \t ${C.yellow}- Find out how to get in touch with me.${C.reset}
                       \r\n${C.red}clear${C.reset} \t\t ${C.yellow}- Clear The Terminal.${C.reset}`,
       about: `\nHi, I'm ${C.red}AOURAR ABDELOUAHAB${C.reset}, Second Year Student At ${C.green}ENSIA${C.reset},${C.cyan} Graphic Designer${C.reset} And ${C.cyan}Web Developer${C.reset}.\r\nI enjoy crafting interactive user interfaces and building ${C.green}fast${C.reset}, ${C.green}accessible${C.reset}, and ${C.green}maintainable${C.reset} web applications.\r\nI focus on writing ${C.yellow}clean${C.reset}, ${C.yellow}efficient code${C.reset} and continuously learning ${C.yellow}new tools${C.reset} and ${C.yellow}best practices${C.reset}.\r\nWhen I'm not coding, you'll find me exploring ${C.cyan}new tech trends${C.reset}, or improving ${C.cyan}my personal portfolio.${C.reset}`,
-      projects: `\n${C.red}RedVolt Gear${C.reset} \t\t\t - RedVolt Gear is a sleek, animated ${C.yellow}React-based e-commerce website${C.reset} built to showcase and sell high-quality ${C.yellow}laptop accessories${C.reset}.\r\n\n${C.red}Tic Tac Toe GUI${C.reset} \t\t - Tic Tac Toe ${C.yellow}Game${C.reset} implemented using ${C.yellow}C++${C.reset} with a ${C.yellow}graphical user interface${C.reset} using the library ${C.yellow}SDL2${C.reset}, Co-Created with TRARI MOHAMMED EL MEHDI.\r\n\n${C.red}Portfolio (Terminal Theme)${C.reset} \t - A developer portfolio built using ${C.yellow}React${C.reset}, ${C.yellow}TypeScript${C.reset}, and ${C.yellow}xterm.js${C.reset}.Styled with ${C.yellow}TailwindCSS${C.reset}, this terminal interface mimics a command-line experience for showcasing my work and skills in a fun, interactive way.`,
+      projects: `\n${C.red}RedVolt Gear${C.reset} \t\t\t - RedVolt Gear is a sleek, animated ${C.yellow}React-based e-commerce website${C.reset} built to showcase and sell high-quality ${C.yellow}laptop accessories${C.reset}.\r\n\n${C.red}Tic Tac Toe GUI${C.reset} \t\t - Tic Tac Toe ${C.yellow}Game${C.reset} implemented using ${C.yellow}C++${C.reset} with a ${C.yellow}graphical user interface${C.reset} using the library ${C.yellow}SDL2${C.reset}, Co-Created with TRARI MOHAMMED EL MEHDI (${C.red}His Github:${C.reset}${C.green}${underline}https://github.com/arialCoding${reset}${C.reset}).\r\n\n${C.red}Portfolio (Terminal Theme)${C.reset} \t - A developer portfolio built using ${C.yellow}React${C.reset}, ${C.yellow}TypeScript${C.reset}, and ${C.yellow}xterm.js${C.reset}.Styled with ${C.yellow}TailwindCSS${C.reset}, this terminal interface mimics a command-line experience for showcasing my work and skills in a fun, interactive way.
+      \r\n${C.cyan}RedVolt Gear Repo${C.reset}\t ${C.green}${underline}https://github.com/Abdelouahab-aourar/RedVolt-Gear${reset}${C.reset}
+      \r\n${C.cyan}Tic Tac Toe Repo${C.reset}\t ${C.green}${underline}https://github.com/Abdelouahab-aourar/TicTacToeGui${reset}${C.reset}
+      \r\n${C.cyan}My Portfolio Repo${C.reset}\t ${C.green}${underline}https://github.com/Abdelouahab-aourar/My-Portfolio${reset}${C.reset}`,
       skills: `\n${C.green}Here are the tools and technologies I work with:${C.reset}\n
 \r${C.cyan}Languages:${C.reset}\n
 \r${C.yellow}- C++${C.reset}
@@ -63,10 +68,10 @@ const TerminalComponent = () => {
 \r${C.yellow}- Adobe Illustrator${C.reset}`,
       contact: `\n${C.green}Feel free to reach out! Here’s how you can contact me:${C.reset}\n
 
-\r${C.cyan}Email${C.reset}\t\t${C.yellow}abdelouahab.aourar@gmail.com${C.reset}
-\r${C.cyan}GitHub${C.reset}\t\t${C.yellow}https://github.com/Abdelouahab-aourar${C.reset}
-\r${C.cyan}LinkedIn${C.reset}\t${C.yellow}https://www.linkedin.com/in/aourar-abdelouahab${C.reset}
-\r${C.cyan}Instagram${C.reset}\t${C.yellow}https://www.instagram.com/abdelouahab_aourar/profilecard/?igsh=NnpmNGJ5b3M0YXhi${C.reset}\n
+\r${C.cyan}${underline}Email${reset}${C.reset}\t\t${C.yellow}${underline}abdelouahab.aourar@gmail.com${reset}${C.reset}
+\r${C.cyan}${underline}GitHub${reset}${C.reset}\t\t${C.yellow}${underline}https://github.com/Abdelouahab-aourar${reset}${C.reset}
+\r${C.cyan}${underline}LinkedIn${reset}${C.reset}\t${C.yellow}${underline}https://www.linkedin.com/in/aourar-abdelouahab${reset}${C.reset}
+\r${C.cyan}${underline}Instagram${reset}${C.reset}\t${C.yellow}${underline}https://www.instagram.com/abdelouahab_aourar/profilecard/?igsh=NnpmNGJ5b3M0YXhi${reset}${C.reset}\n
 \rType ${C.red}about${C.reset} or ${C.red}projects${C.reset} to learn more about what I do.`,
       clear: "__CLEAR__",
     };
@@ -75,7 +80,7 @@ const TerminalComponent = () => {
     let histIdx = -1;
 
     const prompt = () => term.write(`${C.red}\r\nabdelouahab@portfolio$> ${C.reset}`);
-    term.writeln("Welcome To My Portflio! Type help for more info");
+    term.writeln(`Welcome To My Portflio! Type ${C.red}help${C.reset} for more information`);
     prompt();
 
     term.onData((data) => {
